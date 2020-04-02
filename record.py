e14f492d18b3216d391d8b5b399e81187988ae45
Bui Quang Ha <buiquangha250199@gmail.com>
2020-04-02 14:15:05 +0700

    update

--- record.py
+++ record.py
@@ -2,24 +2,33 @@ import librosa
 import soundfile as sf
 import sounddevice as sd
 import time
-import keyboard
-
+import keyboard    
 def sync_record(filename, duration, fs, channels):
  print('Recording...')
  myrecording = sd.rec(int(duration * fs), samplerate=fs, channels=channels)
  sd.wait()
  sf.write(filename, myrecording, fs)
- print('Done recording!')
+ print('Done recording  !')
 
+f = open("demo.txt", "r")
+print("Speak the sentence below: ")
+print("---------------------------------------------")
+print(f.read())
+print("---------------------------------------------")
 
-print('################################################################')
+print('#############################################')
 print('Please press SPACE to start recording!')
+print('###########################')
+print('#####################')
 print('################')
+print('##########') 
+print('######')
 
 while True:  # making a loop
     try:  # used try so that if user pressed other than the given key error will not be shown
         if keyboard.is_pressed('space'):  # if key 'q' is pressed 
-            sync_record('sync_record.wav', 10, 16000, 1)
+            sync_record('record_data/Tâm sự/record2.wav', 15, 16000, 1) 
             break  # finishing the loop
     except:
         break  # if user pressed a key other than the given key the loop will break
+        
